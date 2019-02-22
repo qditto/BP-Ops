5424774ca8c05eb39a8450110385fe624184ab1a
ditto <trevor@jumpsixmarketing.com>
2019-02-22 14:45:14 -0600

    add client logins to client view

--- resources/js/pages/ClientView.vue
+++ resources/js/pages/ClientView.vue
@@ -4,14 +4,19 @@
             <span style="width: 80%">
                 {{client.name}}
             </span>
-            <router-link tag="button" :to="{path: edit_url}" :class="'btn mr-1 mb-1 btn-success'" style="color: #fff">Edit</router-link>
+            <router-link tag="button" :to="{path: edit_url}" :class="'btn mr-1 mb-1 btn-success'" style="color: #fff">
+                Edit
+            </router-link>
         </div>
         <div class="row">
             <div class="col-6">
                 <div class="card card-default border-primary">
                     <div class="card-header text-white bg-primary">
                         Products
-                        <div class="card-tool float-right"><router-link tag="em" :class="'fas fa-edit text-white'" :to="{path: '/clients/' + this.$route.params.slug + '/products'}"></router-link></div>
+                        <div class="card-tool float-right">
+                            <router-link tag="em" :class="'fas fa-edit text-white'"
+                                         :to="{path: '/clients/' + this.$route.params.slug + '/products'}"></router-link>
+                        </div>
                     </div>
                     <div class="card-body">
                         <ul>
@@ -28,8 +33,33 @@
                     <div class="card-header text-white bg-success">
                         Logins
                     </div>
-                    <div class="card-body">
-                        <span v-if="logins">Test</span>
+                    <div class="card-body text-center">
+                        <div v-if="logins">
+                            <b-tabs>
+                                <b-tab v-for="(login, login_index) in client.logins" :key="login.id" :title="login.name" :active="login_index === 0">
+                                    <div class="row bb  p-2">
+                                        <div class="col-6">
+                                            <h3 class="m-0">User Name</h3>
+                                            <p class="m-0">{{login.username}}</p>
+                                        </div>
+                                        <div class="col-6">
+                                            <h3 class="m-0">Password</h3>
+                                            <p class="m-0">{{login.password}}</p>
+                                        </div>
+                                    </div>
+                                    <div class="row p-2">
+                                        <div class="col-6">
+                                            <h3 class="m-0">URL</h3>
+                                            <p class="m-0">{{login.url}}</p>
+                                        </div>
+                                        <div class="col-6">
+                                            <h3 class="m-0">Notes</h3>
+                                            <p class="m-0">{{login.notes}}</p>
+                                        </div>
+                                    </div>
+                                </b-tab>
+                            </b-tabs>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -43,7 +73,7 @@
         data() {
             return {
                 client: '',
-                edit_url : '/clients/' + this.$route.params.slug + '/edit',
+                edit_url: '/clients/' + this.$route.params.slug + '/edit',
                 logins: false
             }
         },
@@ -52,7 +82,7 @@
             axios.get('api/clients/' + this.$route.params.slug)
                 .then(function (res) {
                     self.client = res.data
-                    if(self.client.logins.length > 0){
+                    if (self.client.logins.length > 0) {
                         self.logins = true
                     }
                 })
